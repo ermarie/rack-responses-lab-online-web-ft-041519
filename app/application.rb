7f5e107b0d3902db53b9cca7ea579f2db14a9839
Erin Stalzer <erinstalzer@gmail.com>
2019-05-29 20:25:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,15 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
+    time = Time.new
+
+    if time.hour > 11
+      resp.write "Morning"
+    else
+      resp.write "Afternoon"
+
+    end
+
     # if num_1==num_2 && num_2==num_3
     #   resp.write "You Win"
     # else
